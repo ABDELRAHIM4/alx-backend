a9592e41604e7b7ec68b3636e3f7cae1ede1395d
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-08-19 21:11:13 +0000

    backend

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -3,7 +3,7 @@ import math
 from typing import List
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> int:
     """index_range that takes two integer arguments page and page_size"""
     start = (page - 1) * page_size
     end = start + page_size
