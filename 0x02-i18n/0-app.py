1fe120db1344e15775acd81e5968e5797357cdd0
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-08-27 23:25:46 +0000

    backend

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,7 +3,14 @@
 
 
 from flask import Flask, render_template
+from flask_babel import Babel
 app = Flask(__name__)
+babel = Babel(app)
+class Config():
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+app.config.from_object(Config)
 
 @app.route("/")
 def begin():
